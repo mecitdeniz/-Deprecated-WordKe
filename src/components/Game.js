eef0e60f004788a7a6876a51b090caf29221438e
mecitdeniz <mecitdenz@gmail.com>
2020-10-17 01:25:01 +0300

    game board design

--- src/components/Game.js
+++ src/components/Game.js
@@ -14,6 +14,7 @@ import {
 import { useSelector, useDispatch } from 'react-redux';
 
 import CoinBar from './CoinBar';
+import Tile from './Tile';
 import AddBar from './AddBar';
 
 const Game = ({navigation}) =>{
@@ -30,6 +31,29 @@ const Game = ({navigation}) =>{
                 coins={ coins }
                 diamonds={ diamonds }
             />
+            <View style={{flex:1,justifyContent:"center"}}>
+                <View style={styles.gameTable}>
+                    <View style={styles.column}>
+                        <Tile character="A"/>
+                        <Tile character="A"/>
+                        <Tile character="A"/>
+                    </View>
+                    <View style={styles.column}>
+                        <Tile character="A"/>
+                        <Tile character="A"/>
+                        <Tile character="A"/>
+                    </View>
+                    <View style={styles.column}>
+                        <Tile character="A"/>
+                        <Tile character="A"/>
+                        <Tile character="A"/>
+                    </View>
+                    <View style={[styles.column,{justifyContent:"center",paddingTop:10}]}>
+                        <Tile character="A"/>
+                    </View>
+                </View>
+            </View>
+            
             <AddBar
                 isConfigured={isConfigured}
             />
@@ -40,19 +64,23 @@ const Game = ({navigation}) =>{
 
 const styles = StyleSheet.create({
     container:{
-      flex:1
+      flex:1,
     },
-    text:{
-        color:"white",
-        fontSize:30,
-        fontWeight:"bold",
-    },
-    textContainer:{
-      flexDirection:"row",
-      alignItems:"center",
-      height:40,
-      marginRight:5
-    }
+    gameTable:{
+        width:270,
+        height:400,
+        paddingVertical:20,
+        paddingHorizontal:5,
+        alignSelf:"center",
+        borderWidth:0,
+        justifyContent:"space-around"
+      },
+      column:{
+        flexDirection:"row",
+        alignItems:"center",
+        justifyContent:"space-between",
+        height:40,
+      },
   });
 
 export default Game;
