3b0e4eb08c7a2070f24cb2290b5cc5130c55dab5
mecitdeniz <mecitdenz@gmail.com>
2020-10-20 23:12:52 +0300

    check win

--- src/utils/game.js
+++ src/utils/game.js
@@ -1,13 +1,34 @@
-const getGameData =()=>{
+const LEVELS = require('../db/levels.json');
+
+let currentLevel = 0;
+const getPlayerData = () =>{
     return {
-        correctOrder:[["Ş","A","D"],["A","T","İ"],["N","A","Z"]],
-        currentOrder:[["","A","D"],["A","T","İ"],["N","A","Z"]],
+        //currentLevel:2,
+        coins:100,
+        diamonds:20
+    }
+}
+
+const getGameData =()=>{
+
+    //const currentLevel = getPlayerData().currentLevel;
+    console.log()
+    return {
+        correctOrder:LEVELS[currentLevel].goal,
+        currentOrder:LEVELS[currentLevel].state,
         lastCharacter:"Ş"
     }
 }
 
-
+const checkOrder = (currentOrder,correctOrder) =>{
+    if(JSON.stringify(currentOrder) === JSON.stringify(correctOrder)){
+        currentLevel++;
+        return true
+    }
+    else return false
+}
 
 module.exports = {
     getGameData,
+    checkOrder
 }
