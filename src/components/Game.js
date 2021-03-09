132279e03bc26022c6febee187e5b4de2ffea10c
mecitdeniz <mecitdenz@gmail.com>
2021-03-10 02:33:44 +0300

    redesign game screen

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,125 +1,114 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import {
-    View,
-    SafeAreaView,
-    StatusBar,
-    StyleSheet,
-    Alert,
-    ImageBackground,
-    BackHandler,
+  View,
+  Alert,
+  BackHandler,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
 } from 'react-native';
-import { useSelector, useDispatch } from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 
 import CoinBar from './CoinBar';
 import Tile from './Tile';
 import AddBar from './AddBar';
 import WinAlert from './WinAlert';
-import { unSelect } from '../store/actions';
+import {unSelect} from '../store/actions';
 
-
-const Game = ({navigation}) =>{
-
-  const data = useSelector(state=>state);
-  const { coins, diamonds, isWin } = data;
+const Game = ({navigation}) => {
+  const data = useSelector((state) => state);
+  const {coins, diamonds, isWin} = data;
   const dispatch = useDispatch();
 
   useEffect(() => {
     const backAction = () => {
-      Alert.alert("Hold on!", "Are you sure you want to go back?", [
+      Alert.alert('Hold on!', 'Are you sure you want to go back?', [
         {
-          text: "Cancel",
+          text: 'Cancel',
           onPress: () => null,
-          style: "cancel"
+          style: 'cancel',
         },
-        { text: "YES", onPress: () => {
-            dispatch(unSelect())
-            navigation.goBack()
-        } }
+        {
+          text: 'YES',
+          onPress: () => {
+            dispatch(unSelect());
+            navigation.goBack();
+          },
+        },
       ]);
       return true;
     };
 
     const backHandler = BackHandler.addEventListener(
-      "hardwareBackPress",
-      backAction
+      'hardwareBackPress',
+      backAction,
     );
 
     return () => backHandler.remove();
   }, []);
-  return(
+  return (
     <SafeAreaView style={styles.container}>
-        <StatusBar hidden/>
-        <ImageBackground source={require('../assets/w1.png')} style={styles.container}>
-            <CoinBar
-                coins={ coins }
-                diamonds={ diamonds }
-            />
-            
-            <View style={{flex:1,justifyContent:"center"}}>
-                {renderContent(isWin)}
-            </View>
+      <StatusBar hidden />
+      <View style={styles.container}>
+        <CoinBar coins={coins} diamonds={diamonds} />
 
-            <AddBar/>
-        </ImageBackground>
+        <View style={{flex: 1, justifyContent: 'center'}}>
+          {renderContent(isWin)}
+        </View>
+
+        <AddBar />
+      </View>
     </SafeAreaView>
   );
-}
+};
 
-
-const renderContent = (isWin) =>{
-  if(isWin) return(<WinAlert showAlert={true}/>)
-  else{
-    return(
+const renderContent = (isWin) => {
+  if (isWin) return <WinAlert showAlert={true} />;
+  else {
+    return (
       <View>
         <View style={styles.gameTable}>
           <View style={styles.column}>
-            <Tile row={0} col={0}/>
-            <Tile row={0} col={1}/>
-            <Tile row={0} col={2}/>
+            <Tile row={0} col={0} />
+            <Tile row={0} col={1} />
+            <Tile row={0} col={2} />
           </View>
           <View style={styles.column}>
-            <Tile row={1} col={0}/>
-            <Tile row={1} col={1}/>
-            <Tile row={1} col={2}/>
+            <Tile row={1} col={0} />
+            <Tile row={1} col={1} />
+            <Tile row={1} col={2} />
           </View>
           <View style={styles.column}>
-            <Tile row={2} col={0}/>
-            <Tile row={2} col={1}/>
-            <Tile row={2} col={2}/>
+            <Tile row={2} col={0} />
+            <Tile row={2} col={1} />
+            <Tile row={2} col={2} />
           </View>
         </View>
-        
       </View>
     );
   }
+};
 
-}
-
-/*
-<View style={[styles.column,{justifyContent:"center",paddingTop:10}]}>
-          <Text style={{color:"white",fontSize:20,fontWeight:"bold"}}>{selectedRow}</Text>
-          <Text style={{color:"white",fontSize:20,fontWeight:"bold"}}>{selectedCol}</Text>
-        </View>
-*/
 const styles = StyleSheet.create({
-    container:{
-      flex:1,
-    },
-    gameTable:{
-        width:270,
-        height:350,
-        paddingVertical:20,
-        paddingHorizontal:5,
-        alignSelf:"center",
-        borderWidth:0,
-        justifyContent:"space-around"
-      },
-      column:{
-        flexDirection:"row",
-        alignItems:"center",
-        justifyContent:"space-between",
-        height:40,
-      },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#3698A6',
+  },
+  gameTable: {
+    width: 270,
+    height: 350,
+    paddingVertical: 20,
+    paddingHorizontal: 5,
+    alignSelf: 'center',
+    borderWidth: 0,
+    justifyContent: 'space-around',
+  },
+  column: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: 40,
+  },
+});
 
 export default Game;
